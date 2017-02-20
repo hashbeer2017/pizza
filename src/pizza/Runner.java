fffacfc22b18e12e6d3a3ef85e7f31e6adfee104
Domenico Vitarella <Domenico Vitarella>
2017-02-20 17:11:01 +0100

    modifica dom

--- src/pizza/Runner.java
+++ src/pizza/Runner.java
@@ -3,7 +3,7 @@ package pizza;
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Hashbeer team! :)");
+		System.out.println("Hello Hashbeer team!");
 	}
 
 }
