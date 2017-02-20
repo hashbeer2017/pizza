840c22ce88527c4bba874074e00df7c840698ce4
Giuseppe Lorusso <Giuseppe Lorusso>
2017-02-20 20:32:28 +0100

    split implementato, da testare meglio forse

--- src/pizza/Runner.java
+++ src/pizza/Runner.java
@@ -1,9 +1,20 @@
 package pizza;
 
+import matrix.Matrix;
+import matrix.MyMatrix;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Hashbeer team!");
+		// Inizializzare la matrice
+		//Matrix<Object> game = new MyMatrix<Object>("");
+		
+		// Associare un rect alla matrice
+		
+		// Effettuare lo split, considerare solo i nodi foglia.
+		
+		// Verificare se i nodi foglia vanno presi o vanno scartati
+		
 	}
 
 }
